7c817383c2b4e0bbc5ab9524f35ae8199394c4c2
markderry <mark.derry@icloud.com>
2025-09-16 23:17:11 -0600

    Add initial composer and registration files for Lightroom export plugin

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+// registration.php
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'MarkDerry_LightroomExportApplePhotos',
+    __DIR__
+);
